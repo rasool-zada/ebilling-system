cf2c2b5fd560ae19062599ed68eb90a99ec437f2
rasool-zada <rasool-zada@gmail.com>
2022-07-24 22:28:08 +0500

    Project Code is Ready

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div class="container">
+<hr>
+<p class="centered">Copyright &copy; Group No.9 Web Engineering Lab 2022</p>
+</div>
---
+++ user/footer.php
@@ -0,0 +1,8 @@
+<!-- FOOTER -->
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+            Copyright &copy; Group No.9 Web Engineering Lab 2022
+            </div>
+        </div>
+    </footer>
